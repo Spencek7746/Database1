96f43b140459b394fbd90eca29ec68dbd9445f82
spebkarp <spebkarp@EPSG1-825021>
2023-02-14 14:29:35 -0600

    Add project files.

---
+++ Database1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Data.OleDb.OleDbConnection cn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cn = new System.Data.OleDb.OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source =| DataDirectory |\\Database.accdb");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+
+            cn.Open();
+
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+
+            while (read.Read())
+            {
+                data += read.GetString(0) + "\n";
+            }
+        }
+    }
+}
